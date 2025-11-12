2ea4d12af77daa0224c05de2246a0216048b5624
JsAoXn <114832445+JsAoXn@users.noreply.github.com>
2025-11-11 19:51:49 -0500

    damn

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
+
 /* Get references to DOM elements */
 const categoryFilter = document.getElementById("categoryFilter");
 const productsContainer = document.getElementById("productsContainer");
 const chatForm = document.getElementById("chatForm");
 const chatWindow = document.getElementById("chatWindow");
+const SPList = document.getElementById("selectedProductsList");
+
+window.addEventListener("load", async (ev) => {
+  const products = await loadProducts();
+
+
+  const filteredProducts = products.filter(
+    (product) => localStorage.getItem(product.name) != null
+  );
+
+  SPList.innerHTML = filteredProducts
+    .map(
+      (product) => `
+    <div class="product-card" id="S${product.name}">
+      <img src="${product.image}" alt="${product.name}">
+      <div class="product-info">
+        <h3>${product.name}</h3>
+        <p>${product.brand}</p>
+      </div>
+    </div>
+  `
+    )
+    .join("");
+
+  const cards = SPList.querySelectorAll(".product-card");
+  cards.forEach((card) => {
+    card.classList.add("highlight")
+    card.addEventListener("click", (e) => {
+      current = e.currentTarget;
+
+      Name = current.id.slice(1)
+      current.remove()
+      localStorage.removeItem(Name)
+      prod = document.getElementById(Name)
+      prod.classList.remove("highlight")
+
+    })
+  })
+})
 
 /* Show initial placeholder until user selects a category */
 productsContainer.innerHTML = `
@@ -19,11 +59,11 @@ async function loadProducts() {
 }
 
 /* Create HTML for displaying product cards */
-function displayProducts(products) {
+async function displayProducts(products) {
   productsContainer.innerHTML = products
     .map(
       (product) => `
-    <div class="product-card">
+    <div class="product-card" id="${product.name}">
       <img src="${product.image}" alt="${product.name}">
       <div class="product-info">
         <h3>${product.name}</h3>
@@ -33,12 +73,84 @@ function displayProducts(products) {
   `
     )
     .join("");
+
+
+
+  const cards = productsContainer.querySelectorAll(".product-card");
+  cards.forEach(async (card) => {
+    descArea = document.querySelector(".search-section");
+
+    if (localStorage.getItem(card.id) != null) {
+      card.classList.add("highlight");
+    }
+    card.addEventListener("click", (e) => {
+      current = e.currentTarget;
+
+      if (current.classList.contains("highlight")) {
+        current.classList.remove("highlight");
+        s = document.getElementById(`S${current.id}`)
+        s.remove()
+        localStorage.removeItem(current.id)
+      }
+      else {
+        current.classList.add("highlight");
+        localStorage.setItem(current.id, "");
+        el = current.cloneNode(true);
+
+        SPList.appendChild(el);
+        el.id = `S${current.id}`
+        el.addEventListener("click", (e) => {
+          current.classList.remove("highlight");
+          s = document.getElementById(`S${current.id}`)
+          s.remove()
+          localStorage.removeItem(current.id)
+        })
+      }
+    })
+    description = document.createElement("p")
+
+
+
+    const filteredProducts = products.filter(
+      (product) => product.name === card.id
+    );
+
+    description.textContent = filteredProducts[0]["description"];
+    description.classList.add("visually-hidden");
+    description.classList.add("description");
+    description.id = `D${filteredProducts[0]["name"]}`
+    descArea.appendChild(description);
+
+
+    card.addEventListener("mouseenter", (e) => {
+
+      temp = document.getElementById(`D${card.id}`)
+      temp.classList.remove("visually-hidden")
+    })
+
+    card.addEventListener("mouseleave", (e) => {
+      temp = document.getElementById(`D${card.id}`)
+      temp.classList.add("visually-hidden")
+    })
+
+
+  });
+
+
+
 }
+
+
 
 /* Filter and display products when category changes */
 categoryFilter.addEventListener("change", async (e) => {
   const products = await loadProducts();
   const selectedCategory = e.target.value;
+
+  descriptions = document.getElementsByClassName("description");
+  while (descriptions[0]) {
+    descriptions[0].remove();
+  }
 
   /* filter() creates a new array containing only products 
      where the category matches what the user selected */
@@ -55,3 +167,4 @@ chatForm.addEventListener("submit", (e) => {
 
   chatWindow.innerHTML = "Connect to the OpenAI API for a response!";
 });
+
